49a6636b3b4eebc25af38785ddb833d039879d19
joshayres <me@joshayres.tech>
2021-11-29 14:31:27 -0800

    more testing

--- testing/test.js
+++ testing/test.js
@@ -1,7 +1,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+  http.get('http://localhost:3000/products/' + getRandomInt(1, 1000011) + '/styles');
 }
