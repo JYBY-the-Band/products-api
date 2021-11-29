dbe5a3f44a728cae534cf7524746903a692720f9
joshayres <me@joshayres.tech>
2021-11-29 12:11:00 -0800

    testing setup

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('newrelic');
 const { sequelize } = require('./db/models');
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(cors());
 
 const routes = require('./routes');
