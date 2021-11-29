49fba42c4edbe9a2890cb2fa48822aeebb2666ea
joshayres <me@joshayres.tech>
2021-11-29 10:33:30 -0800

    all routes not completed

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const { Op } = require('sequelize');
-const { Product } = require('../db/models');
+const { Product, Style, RelatedItem } = require('../db/models');
 var express = require('express')
 var router = express.Router()
 
@@ -23,6 +23,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:product_id', (req, res) => {
+  // TODO: also get features
   Product.findAll({
     where: {
       id: req.params.product_id
@@ -30,6 +31,41 @@ router.get('/:product_id', (req, res) => {
   })
     .then(data => {
       res.send(data).status(200);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(err).status(500);
+    });
+});
+
+router.get('/:product_id/styles', (req, res) => {
+  // TODO: also get photos and skus
+  Style.findAll({
+    where: {
+      ProductId: req.params.product_id
+    }
+  })
+    .then(data => {
+      res.send(data).status(200);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(err).status(500);
+    });
+});
+
+router.get('/:product_id/related', (req, res) => {
+  RelatedItem.findAll({
+    where: {
+      current_product_id: req.params.product_id
+    }
+  })
+    .then(data => {
+      let result = [];
+      data.forEach(item => {
+        result.push(item.related_product_id);
+      })
+      res.send(result).status(200);
     })
     .catch(err => {
       console.error(err);
